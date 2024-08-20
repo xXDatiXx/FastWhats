17206c58b96ff4853dc79e8761836e8a18b911dc
Dati <102495187+xXDatiXx@users.noreply.github.com>
2024-08-19 19:44:39 -0600

    ejecutable

--- fastWhats.py
+++ fastWhats.py
@@ -1,44 +1,46 @@
 import webbrowser
 import tkinter as tk
 import time
-#import pyautogui
+#import keyboard
+import time
 
-#Funcion para abrir whatsapp web
+#Funcion para abrir whatsapp
 def abrirWhatsapp(num, opc):
     if opc == 1:
         webbrowser.open("https://web.whatsapp.com/send?phone=" + num)
         time.sleep(8)
-        pyautogui.write("Hola de Clean Walkers Naucalpan, tu servicio ya esta listo! ")
-        pyautogui.press('enter')
+        #keyboard.write("Hola de Clean Walkers Naucalpan, tu servicio ya esta listo!")
+        #keyboard.press_and_release('enter')
     elif opc == 0:
         webbrowser.open("https://web.whatsapp.com/send?phone=" + num)
+
 
 #Ventana
 ventana = tk.Tk()
 ventana.configure(bg='#ffffff')
 ventana.title("Fast Whatsapp by Dati")
-ventana.geometry("500x200")
+ventana.geometry("800x300")
 ventana.resizable(False,False)
 
 #Label
-label = tk.Label(ventana, bg='#ffffff', text="Ingrese el numero de telefono: ")
+label = tk.Label(ventana, bg='#ffffff', text="Ingrese el numero de telefono: ", font=("Helvetica", 24))
 label.place(x=10, y=10)
 
 #entry para pedir el numero 
-num = tk.Entry(ventana)
-num.place(x=300,y=10)
+num = tk.Entry(ventana, font=("Helvetica", 20))
+num.place(x=460, y=15)
 entrada = num.get()
         
-#Boton para abrir whatsapp
-boton = tk.Button(ventana, bg='#3fc150',text="Abrir Whatsapp",command=lambda:abrirWhatsapp(num.get(), 0))
-boton.place(x=300,y=50)
+# Boton para abrir whatsapp
+boton = tk.Button(ventana, bg='#3fc150', text="Abrir Whatsapp", font=("Helvetica", 16), width=20, height=2, command=lambda: abrirWhatsapp(num.get(), 0))
+boton.place(x=500, y=80)
 
-#Boton para abrir whatsapp y mandar mensaje
-boton2 = tk.Button(ventana, bg='#3fc150',text="Enviar mensaje",command=lambda:abrirWhatsapp(num.get(), 1))
-boton2.place(x=300,y=100)
+# Boton para abrir whatsapp y mandar mensaje
+boton2 = tk.Button(ventana, bg='#3fc150', text="Enviar mensaje", font=("Helvetica", 16), width=20, height=2, command=lambda: abrirWhatsapp(num.get(), 1))
+boton2.place(x=500, y=160)
 
 #Imagen
-img = tk.PhotoImage(file="logoCW.png")
+img = tk.PhotoImage(file="LogoCW.png")
 labelImg = tk.Label(ventana, bg='#ffffff' , image=img)
 labelImg.place(x=10,y=40)
 
